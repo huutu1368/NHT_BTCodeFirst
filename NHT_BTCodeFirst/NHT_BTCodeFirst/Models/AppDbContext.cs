07a41dde4366196b4c957743f52cc7ad1b9230d9
Huu Tu <huutu1368@gmail.com>
2026-04-10 21:01:32 +0700

    NguyenHuuTu_Step8_AddLayout

---
+++ NHT_BTCodeFirst/NHT_BTCodeFirst/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NHT_BTCodeFirst.Models;
+
+namespace NHT_BTCodeFirst.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ NHT_BTCodeFirst/NHT_BTCodeFirst/Models/Grade.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace NHT_BTCodeFirst.Models
+{
+    public class Grade
+    {
+        [Key]
+        public int GradeId { get; set; }
+        [Required]
+        public string GradeName { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}

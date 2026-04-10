94d87e6a145b0e9295b5485638822dc36348aeb0
Huu Tu <huutu1368@gmail.com>
2026-04-10 20:26:16 +0700

    NguyenHuuTu_Step4_ConfigureDatabaseConnection

---
+++ NHT_BTCodeFirst/NHT_BTCodeFirst/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+namespace NHT_BTCodeFirst.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        [Required]
+        public int GradeId { get; set; }
+
+        [ForeignKey("GradeId")]
+        public virtual Grade Grade { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}

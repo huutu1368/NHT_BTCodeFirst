2368a97b94518ab1f1628d62cfcfc584990a24ea
Huu Tu <huutu1368@gmail.com>
2026-04-10 20:34:07 +0700

    NguyenHuuTu_Step5_DangKyDbContext

---
+++ NHT_BTCodeFirst/NHT_BTCodeFirst/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NHT_BTCodeFirst.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
